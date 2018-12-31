84ffbc6dc43772f6e661c9bee29d23ac5ba0983f
fabiohsgomes <fabiohsalesgomes@gmail.com>
2018-12-31 11:03:43 -0200

    Organizando codigo, closure e servico

--- public/app/app.js
+++ public/app/app.js
@@ -1,9 +1,15 @@
-import {handleStatus} from './utils/promise-helpers.js';
+import {log} from './utils/promise-helpers.js';
+import {notasService as service} from './nota/service.js';
+
+import './utils/array-helpers.js';
+
+const sumItens = code => notas => notas.$flatMap(nota=>nota.itens)
+.filter(item=> item.codigo == code)
+.reduce((total, item) => total+item.valor,0);
 
 document.querySelector('#myButton')
 .addEventListener('click', ( e ) => {
-    fetch('http://localhost:3000/notas')
-    .then(handleStatus)
-    .then(notas => console.log(notas))
-    .catch(err => console.log(err));
-})
+    service.sumItens('2143')
+    .then(log)
+    .catch(log);
+});
