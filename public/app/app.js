43bfc57302f7d6672f5945a489a33f3fff785a73
fabiohsgomes <fabiohsalesgomes@gmail.com>
2019-01-01 16:51:50 -0200

    Finalizando projeto

--- public/app/app.js
+++ public/app/app.js
@@ -1,14 +1,14 @@
 import {log, timeoutPromise, delay, retry} from './utils/promise-helpers.js';
 import {notasService as service} from './nota/service.js';
 import {takeUtil, debounceTime, partialize, pipe} from './utils/operators.js';
+import {EventEmmiter} from './utils/event-emmiter.js';
 
 import './utils/array-helpers.js';
 
 const operations = pipe(partialize(takeUtil,3), partialize(debounceTime,500));
 
 const action = operations(() => retry(3,3000,()=>timeoutPromise(200,service.sumItems('2143')))
-.then(delay(5000))
-.then(log)
+.then(total => EventEmmiter.emmit('itensTotalizados', total))
 .catch(log));
 
 document.querySelector('#myButton').addEventListener('click',action);
--- public/app/nota/service.js
+++ public/app/nota/service.js
@@ -1,18 +1,21 @@
 import {handleStatus} from '../utils/promise-helpers.js';
 import {partialize, pipe} from '../utils/operators.js';
+import {Maybe} from '../utils/maybe.js';
+
 const API = 'http://localhost:3000/notas';
 
-const getItemsFromNotas = notas => notas.$flatMap(nota=>nota.itens);
-const filterItemsByCode = (code, items) => items.filter(item => item.codigo === code);
-const sumItemsValue = items => items.reduce((total, item)=>total+item.valor,0);
+const getItemsFromNotas = notasM => notasM.map(notas=>notas.$flatMap(nota=>nota.itens));
+const filterItemsByCode = (code, itemsM) => itemsM.map(items.filter(item => item.codigo === code));
+const sumItemsValue = itemsM => itemsM.map(items.reduce((total, item)=>total+item.valor,0));
 
 export const notasService = {
     listAll(){
         return fetch(API)
         .then(handleStatus)
+        .then(notas => Maybe.of(notas))
         .catch(err => {
             log(err);
-            return Promise.reject('Não foi possível obter as notas fiscais');
+            return Promise.reject('Nï¿½o foi possï¿½vel obter as notas fiscais');
         })
     },
     sumItems(code){
